f518ad300d63964330a54846539145c95ea370f1
Danilo Freire <154559134+danilofreeire@users.noreply.github.com>
2025-10-26 19:06:04 +0000

    client persist and server multiclient

--- UDPClient.py
+++ UDPClient.py
@@ -5,33 +5,44 @@ SERVER_NAME = 'localhost'
 SERVER_PORT = 12000
 
 def main():
-    
+
   clientSocket = socket(AF_INET, SOCK_DGRAM)
   serverAddress = (SERVER_NAME, SERVER_PORT)
 
   sequence_number = 1
   window_size = 10
 
-  message = input('Input lowercase sentence: ').encode()
-
-  packageClient = pack_packet(
-      version=1,
-      flags=FLAG_DATA,
-      seq=sequence_number,
-      ack=0,
-      win=window_size,
-      payload=message
-  )
-
-  clientSocket.sendto(packageClient, serverAddress)
+  print("Digite /quit para sair.  ")
+  while True:
+      
+    message = input('Input lowercase sentence: ').encode()
+    if not message:
+        continue 
+    if message.decode().strip() == '/quit':
+        break
 
 
-  datagram, addr = clientSocket.recvfrom(2048)
-  packageServer = unpack_packet(datagram)
+    packageClient = pack_packet(
+        version=1,
+        flags=FLAG_DATA,
+        seq=sequence_number,
+        ack=0,
+        win=window_size,
+        payload=message
+    )
 
-  print("checksum_ok:", packageServer["checksum_ok"])
-  print("ack recebido:", packageServer["ack"])
-  print("resposta:", packageServer["payload"].decode(errors="ignore"))
+    clientSocket.sendto(packageClient, serverAddress)
+
+
+    datagram, addr = clientSocket.recvfrom(2048)
+    packageServer = unpack_packet(datagram)
+
+
+
+    print("checksum_ok:", packageServer["checksum_ok"])
+    print("ack recebido:", packageServer["ack"])
+    print("resposta:", packageServer["payload"].decode(errors="ignore"))
+    sequence_number += 1
 
   clientSocket.close()
 
